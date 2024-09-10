b555f4096d1bac841e8d163ad13ac8e7f26361cb
Jeff Yanta <jeff@getcode.com>
2024-09-10 12:34:08 -0700

    Limit transfer action amounts to uint32 max

--- generated/go/transaction/v2/transaction_service.pb.validate.go
+++ generated/go/transaction/v2/transaction_service.pb.validate.go
@@ -2948,10 +2948,10 @@ func (m *NoPrivacyTransferAction) Validate() error {
 		}
 	}
 
-	if m.GetAmount() <= 0 {
+	if val := m.GetAmount(); val <= 0 || val > 4294967295 {
 		return NoPrivacyTransferActionValidationError{
 			field:  "Amount",
-			reason: "value must be greater than 0",
+			reason: "value must be inside range (0, 4294967295]",
 		}
 	}
 
@@ -3073,10 +3073,10 @@ func (m *NoPrivacyWithdrawAction) Validate() error {
 		}
 	}
 
-	if m.GetAmount() <= 0 {
+	if val := m.GetAmount(); val <= 0 || val > 4294967295 {
 		return NoPrivacyWithdrawActionValidationError{
 			field:  "Amount",
-			reason: "value must be greater than 0",
+			reason: "value must be inside range (0, 4294967295]",
 		}
 	}
 
@@ -3205,10 +3205,10 @@ func (m *TemporaryPrivacyTransferAction) Validate() error {
 		}
 	}
 
-	if m.GetAmount() <= 0 {
+	if val := m.GetAmount(); val <= 0 || val > 4294967295 {
 		return TemporaryPrivacyTransferActionValidationError{
 			field:  "Amount",
-			reason: "value must be greater than 0",
+			reason: "value must be inside range (0, 4294967295]",
 		}
 	}
 
@@ -3331,10 +3331,10 @@ func (m *TemporaryPrivacyExchangeAction) Validate() error {
 		}
 	}
 
-	if m.GetAmount() <= 0 {
+	if val := m.GetAmount(); val <= 0 || val > 4294967295 {
 		return TemporaryPrivacyExchangeActionValidationError{
 			field:  "Amount",
-			reason: "value must be greater than 0",
+			reason: "value must be inside range (0, 4294967295]",
 		}
 	}
 
@@ -3512,10 +3512,10 @@ func (m *FeePaymentAction) Validate() error {
 		}
 	}
 
-	if m.GetAmount() <= 0 {
+	if val := m.GetAmount(); val <= 0 || val > 4294967295 {
 		return FeePaymentActionValidationError{
 			field:  "Amount",
-			reason: "value must be greater than 0",
+			reason: "value must be inside range (0, 4294967295]",
 		}
 	}
 
